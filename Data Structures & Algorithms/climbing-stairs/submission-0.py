27d5cf5d2d7d4140d071fed8c8962f05cbe9f17e
Shubham-Mandal-sudo <shubhammandal205@gmail.com>
2026-04-10 22:04:09 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a = 1
+        b = 1
+        for i in range(2,n+1):
+            b = a+b
+            a = b-a
+        return b
+        
